208c647bdb50d301dad8c2357c0e23d0fbd4d6fc
Sean C. Ylescupidez <seanyles@gmail.com>
2020-02-16 18:50:42 -0500

    Stock Services

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -6,6 +6,7 @@ const PROD = 'http://ec2-54-167-255-136.compute-1.amazonaws.com';
 export const HOSTNAME = PROD + '/api';
 
 export const SEARCH = '/search';
+export const SEARCHTERMS = 'searchTerms';
 
 // User Routes
 export const PROFILE = '/profile';
@@ -16,3 +17,10 @@ export const REGISTER = '/register';
 
 // Recipe Routes
 export const RECIPE = '/recipe';
+
+// Stock Routes
+export const STOCK = '/stock';
+export const STOCK_ITEM = '/stock_item';
+
+// Food Routes
+export const FOOD = '/food';
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import RecipeReducer from './RecipeReducer';
 import UserReducer from './UserReducer';
+import StockReducer from './StockReducer';
 
 const RootReducer = combineReducers({
   recipe: RecipeReducer,
   user: UserReducer,
+  stock: StockReducer,
 });
 
 export default RootReducer;
