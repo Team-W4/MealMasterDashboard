7ff839f8dd8476a3c9385371aa2c19287a115d5c
Sean C. Ylescupidez <seanyles@gmail.com>
2020-02-16 14:33:39 -0500

    User Services

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
 import * as recipeActions from './recipeActions';
+import * as userActions from './userActions';
 
 export { recipeActions };
+export { userActions };
---
+++ src/services/UserService.js
@@ -0,0 +1,50 @@
+import * as routes from '../constants/routes';
+import {
+  deleteHelper,
+  getHelper,
+  postHelper,
+  putHelper,
+} from '../utils/fetchHelper';
+
+class UserService {
+  login = (email, password) => {
+    const route = `${routes.USER}${routes.LOGIN}`;
+    return postHelper(route, { email, password });
+  };
+
+  logout = () => {
+    const route = `${routes.USER}${routes.LOGOUT}`;
+    return postHelper(route);
+  };
+
+  register = (email, password) => {
+    const route = `${routes.USER}${routes.REGISTER}`;
+    const body = {
+      email,
+      password,
+      username: email,
+    };
+    return postHelper(route, body);
+  };
+
+  // Not used
+  getProfile = () => getHelper(routes.PROFILE);
+
+  updateProfile = user => putHelper(routes.PROFILE, user);
+
+  deleteProfile = () => deleteHelper(routes.PROFILE);
+
+  getUserById = userId => {
+    const route = `${routes.USER}/${userId}`;
+    return getHelper(route);
+  };
+
+  searchUsers = searchTerms => {
+    const route = `${routes.USER}${routes.SEARCH}/${searchTerms}`;
+    return getHelper(route);
+  };
+}
+
+const singleton = new UserService();
+
+export default singleton;
