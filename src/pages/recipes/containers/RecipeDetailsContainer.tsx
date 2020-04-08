cf3ede0bc540130170297f1f951f6383a0f1a36c
Daniel Nguyen <anhdau.hp@gmail.com>
2020-04-07 22:16:04 -0400

    Implement recipe likes

--- src/pages/recipes/containers/RecipeDetailsContainer.tsx
+++ src/pages/recipes/containers/RecipeDetailsContainer.tsx
@@ -2,16 +2,27 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { recipeActions } from '../../../actions';
+import { User, RecipeDetails } from '../../../constants/dataTypes';
 import { AuthNavigationProps } from '../../auths/AuthStack';
 import RecipeDetailsPage from '../components/RecipeDetailsPage';
 
 type Props = AuthNavigationProps<'RecipeDetails'> & {
-  recipeDetails?: any;
+  userId: number;
+  recipeDetails: RecipeDetails;
   getRecipeById: (id: number) => void;
+  likeRecipe: (recipeId: number) => void;
+  unlikeRecipe: (recipeId: number) => void;
   clearRecipeDetails: () => void;
 };
 
 class RecipeDetailsContainer extends React.Component<Props> {
+  constructor(props: Props) {
+    super(props);
+
+    this.onFavoriteClick = this.onFavoriteClick.bind(this);
+    this.getFavorited = this.getFavorited.bind(this);
+  }
+
   public componentDidMount(): void {
     const {
       route: {
@@ -19,7 +30,29 @@ class RecipeDetailsContainer extends React.Component<Props> {
       },
       getRecipeById,
     } = this.props;
+
     getRecipeById(recipeId);
+  }
+
+  private onFavoriteClick(): void {
+    const { likeRecipe, unlikeRecipe, recipeDetails: { id } } = this.props;
+    const favorited = this.getFavorited();
+
+    if (favorited) {
+      unlikeRecipe(id);
+    } else {
+      likeRecipe(id);
+    }
+  }
+
+  private getFavorited(): boolean {
+    const { userId, recipeDetails: { savedByUsers } } = this.props;
+
+    if (userId && savedByUsers && savedByUsers.length > 0) {
+      return savedByUsers.some((user: User) => user.id === userId);
+    }
+
+    return false;
   }
 
   public render(): JSX.Element {
@@ -27,11 +60,13 @@ class RecipeDetailsContainer extends React.Component<Props> {
 
     return (
       <RecipeDetailsPage
+        favorited={ this.getFavorited() }
+        recipeDetails={ recipeDetails }
+        onFavorite={ this.onFavoriteClick }
         onBack={ () => {
           clearRecipeDetails();
           navigation.pop(1);
         } }
-        recipeDetails={ recipeDetails }
       />
     );
   }
@@ -39,12 +74,15 @@ class RecipeDetailsContainer extends React.Component<Props> {
 
 const mapStateToProps = (state: any) => ({
   recipeDetails: state.recipe.recipeDetails,
+  userId: state.user.profile.id,
 });
 
 const mapDispatchToProps = (dispatch: any) => bindActionCreators(
     {
       getRecipeById: recipeActions.getRecipeById,
       clearRecipeDetails: recipeActions.clearRecipeDetails,
+      likeRecipe: recipeActions.likeRecipe,
+      unlikeRecipe: recipeActions.unlikeRecipe,
     },
     dispatch,
   );
