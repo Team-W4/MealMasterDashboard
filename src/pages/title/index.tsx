3cbf8a855de45cb581bd55330debb03de2904262
Daniel Nguyen <anhdau.hp@gmail.com>
2020-04-09 18:57:29 -0400

    Shamefully removed emojis

--- src/pages/title/index.tsx
+++ src/pages/title/index.tsx
@@ -15,14 +15,14 @@ export type Props = {
 const greetingsGenerator = (): string => {
   const hour = (new Date()).getHours();
   if (hour < 12) {
-    return 'Good Morning! ☀️';
+    return 'Good Morning!';
   }
 
   if (hour < 19) {
-    return 'Good Afternoon! 🌤';
+    return 'Good Afternoon!';
   }
 
-  return 'Good Evening! 🌙';
+  return 'Good Evening!';
 };
 
 const TitleBar: React.FC<Props> = ({ title }) => (
